37a40246cd815ea7cc7943e21feee2863029d58d
Mino Khosravi <mino.khosravi2025@gmail.com>
2026-06-03 13:49:58 +0330

    add task entity and repository

---
+++ src/main/java/com/minoo/taskmanager/entity/Task.java
@@ -0,0 +1,35 @@
+package com.minoo.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status;
+
+    private LocalDate dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
---
+++ src/main/java/com/minoo/taskmanager/entity/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.minoo.taskmanager.entity;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
